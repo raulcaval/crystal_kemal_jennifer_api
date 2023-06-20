b5ce40c5991185cd94a9a880ee0ee8d438e2c667
Raul Cavalcanti <raul.a.s.cavalcanti@gmail.com>
2023-06-20 20:12:04 -0300

    feat: initial commit

---
+++ src/simple_crystal_app.cr
@@ -0,0 +1,17 @@
+# TODO: Write documentation for `SimpleCrystalApp`
+module SimpleCrystalApp
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
+
+require "kemal"
+
+get "/" do
+  "Hello World!"
+end
+
+
+Kemal.run
+
+
