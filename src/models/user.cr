543eb33ecb98d08ccb484fca41cdaacff496007c
Raul Cavalcanti <raul.a.s.cavalcanti@gmail.com>
2023-06-20 21:09:41 -0300

    feat: create model User and migration

---
+++ src/models/user.cr
@@ -0,0 +1,11 @@
+class User < Jennifer::Model::Base
+  with_timestamps
+
+  mapping(
+    id: Primary32,
+    name: String,
+    age: Int32,
+    created_at: Time?,
+    updated_at: Time?,
+  )
+end
